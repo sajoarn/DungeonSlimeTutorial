7c3970a3f864d036eeb5953aa557b4de147256c9
Sandra Arnold <s.arnold372@gmail.com>
2026-01-01 21:52:40 -0600

    Finished chapter 6: drawing textures

--- DungeonSlime/Game1.cs
+++ DungeonSlime/Game1.cs
@@ -46,29 +46,54 @@ public class Game1 : Core
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
-        // TODO: Add your drawing code here
+        // The bounds of the icon within the texture
+        Rectangle iconSourceRect = new Rectangle(0, 0, 128, 128);
+
+        // The bounds of the word mark within the texture
+        Rectangle wordmarkSourceRect = new Rectangle(150, 34, 458, 58);
+
+        Vector2 screenCenter = new Vector2(Window.ClientBounds.Width, 
+                                            Window.ClientBounds.Height) * 0.5f;
+
+        Vector2 wordmarkOrigin = new Vector2(wordmarkSourceRect.Width,
+                                        wordmarkSourceRect.Height) * 0.5f;
+
+        Vector2 iconOrigin = new Vector2(iconSourceRect.Width,
+                                        iconSourceRect.Height) * 0.5f;
         
         // Begin the sprite batch to prepare for rendering.
         SpriteBatch.Begin();
 
-        // Draw the logo texture
+        // Draw the wordmark
         SpriteBatch.Draw(
             _logo,                  // texture
-            new Vector2(            // position
-                Window.ClientBounds.Width,
-                Window.ClientBounds.Height
-            ) * 0.5f,
-            null,                   // source rectangle
+            // position: screen center offset by icon drawing
+            screenCenter + (Vector2.UnitX * iconOrigin),
+            wordmarkSourceRect,     // source rectangle
             Color.Green * 0.5f,     // color mask * opacity
             MathHelper.ToRadians(0),// rotation
-            new Vector2(            // origin
-                _logo.Width,
-                _logo.Height
-            ) * 0.5f,
+            wordmarkOrigin,
             new Vector2(1.0f, 1.0f),// scale
             SpriteEffects.FlipHorizontally |
             SpriteEffects.FlipVertically, // effects
             0.0f                    // layer depth
+        );
+
+        // Draw the logo
+        SpriteBatch.Draw(
+            _logo,
+            // position: left of wordmark and drawn accounting for own icon offset
+            // NOTE: this is bad and confusing. I just wanted to make sure I understand
+            // what's happening with drawing, vectors, and objects
+            screenCenter - (Vector2.UnitX * wordmarkOrigin) 
+                         - (Vector2.UnitX * iconOrigin * 0.5f),
+            iconSourceRect,
+            Color.White,
+            MathHelper.ToRadians(90),
+            iconOrigin,
+            1.0f,
+            SpriteEffects.None,
+            0.0f
         );
 
         // Always end the sprite batch when finished.
